3aaf16414a1ca00466c48ed406ab6d3c10ea595f
Kurt Wilson <git@kurtw.dev>
2025-02-05 00:04:54 -0500

    Revert "Merge branch 'main' into kwilso24"
    This reverts commit 18b56e1f1ea2679b77573482ab0f70fbf4cb16f9, reversing
    changes made to 9662b8a16f94dfe419b93f8e5d9555d194e40bd7.

--- rand_pkhare.py
+++
@@ -1,6 +0,0 @@
-"""This module contains the unit tests for the mergeSort function."""  # Module docstring
-import random
-
-def random_array(size, min_val=1, max_val=20):  # More flexible: size, min, max
-    """Generates a list of random integers."""
-    return [random.randint(min_val, max_val) for _ in range(size)]
--- test_hw2_debugging.py
+++ test_hw2_debugging.py
@@ -1,28 +1,13 @@
 import pytest
 
-from hw2_debugging import merge_sort
+from .hw2_debugging import mergeSort
 
 class TestMergeSort:
     def test_merge_sort(self):
-        assert merge_sort([1, 2, 3]) == [1, 2, 3]
-        assert merge_sort([3, 2, 1]) == [1, 2, 3]
-        assert merge_sort([1, 3, 2]) == [1, 2, 3]
-        assert merge_sort([2, 1, 3]) == [1, 2, 3]
-        assert merge_sort([2, 3, 1]) == [1, 2, 3]
-        assert merge_sort([3, 1, 2]) == [1, 2, 3]
+        assert mergeSort([1, 2, 3]) == [1, 2, 3]
+        assert mergeSort([3, 2, 1]) == [1, 2, 3]
+        assert mergeSort([1, 3, 2]) == [1, 2, 3]
+        assert mergeSort([2, 1, 3]) == [1, 2, 3]
+        assert mergeSort([2, 3, 1]) == [1, 2, 3]
+        assert mergeSort([3, 1, 2]) == [1, 2, 3]
 
-
-def test_merge_sort():
-    #uraycha
-    arr = [6,3,9,0]
-    assert merge_sort(arr) == [0,3,6,9]
-
-def test_reverse_sorted_array():
-    #uraycha
-    arr = [10,9,8,7,6]
-    assert merge_sort(arr) == [6,7,8,9,10]
-
-def test_duplicate_values():
-    #uraycha
-    arr = [7,5,3,7,5,5]
-    assert merge_sort(arr) == [3,5,5,5,7,7]
