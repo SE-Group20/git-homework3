58cffa9c2b80734cc55f50a16a72f73d68637696
Udita Raychaudhury <uraycha@ncsu.edu>
2025-02-04 19:35:32 -0500

    initial code fix and pylint workflow

--- rand.py
+++ rand.py
@@ -1,8 +1,8 @@
-import subprocess
+''' This module is for assigning random values to array '''
+import random
 
 def random_array(arr):
-    shuffled_num = None
-    for i in range(len(arr)):
-        shuffled_num = subprocess.run(["shuf", "-i1-20", "-n1"], capture_output=True)
-        arr[i] = int(shuffled_num.stdout)
+    """iterate through array and assign random int between 1-20"""
+    for idx, _ in enumerate(arr):
+        arr[idx] = random.randint(1, 20)  # Random number between 1 and 20
     return arr
